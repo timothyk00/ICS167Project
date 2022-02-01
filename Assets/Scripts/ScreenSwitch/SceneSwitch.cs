dc300956e0336624370f2bb22a36525b956719f9
doancleon <74682100+doancleon@users.noreply.github.com>
2022-01-31 22:21:40 -0800

    start/end scene + half of weapons

---
+++ Assets/Scripts/ScreenSwitch/SceneSwitch.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Scene1()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void endScene()
+    {
+        SceneManager.LoadScene("Victory");
+    }
+    public void startScene()
+    {
+        SceneManager.LoadScene("Start Scene");
+    }
+}
