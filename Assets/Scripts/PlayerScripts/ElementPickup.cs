322d7d4289a4e064fbb98e8ef37fe993f1ee9926
doancleon <74682100+doancleon@users.noreply.github.com>
2022-02-19 17:41:32 -0800

    proximity check for item pickup

--- Assets/Scripts/PlayerScripts/ElementPickup.cs
+++ Assets/Scripts/PlayerScripts/ElementPickup.cs
@@ -14,8 +14,8 @@ public class ElementPickup : MonoBehaviour
     private float duration;
     private GameObject powerUp;
     private string pickUpButton;
-    private List<Ability> cs;
     private PlayerWeaponController playerWC;
+    private Vector3 currentPos;
 
     // Retrieves the player script of weapon controller to change variables and call its UI update function
     AbilityFactory myFactory = new AbilityFactory(); //create a new abilit/Element Factory
@@ -23,29 +23,32 @@ public class ElementPickup : MonoBehaviour
     void Start()
     {
         pickUpButton = "pickUp" + this.GetComponent<PlayerMovementController>()._playerNum;
-        playerWC = this.GetComponent<PlayerWeaponController>();
-        cs = playerWC.weapons;
-    }
+        playerWC = this.GetComponent<PlayerWeaponController>();    }
 
     void Update()
     {
+        float distance = Vector3.Distance(currentPos, this.transform.position);
+        if (distance < 10)
+        {
+            duration = pickUpTimer;
+        }
         if (canPickup && Input.GetButtonDown(pickUpButton))
         {
-            Debug.Log("working");
-            if (cs.Count == 2)
+            if (playerWC.weapons.Count == 2)
             {
-                cs = new List<Ability>() { cs[0], myFactory.GetAbility(elementType) }; //Updates the players list of weaponries with the given ability from the factory.
+                playerWC.weapons = new List<Ability>() { playerWC.weapons[0], myFactory.GetAbility(elementType) }; //Updates the players list of weaponries with the given ability from the factory.
                 playerWC.UpdateWeapons(); //Updates the UI
             }
             else
             {
-                if (cs.IndexOf(myFactory.GetAbility(elementType)) == -1)
-                    cs.Add(myFactory.GetAbility(elementType));
+                if (playerWC.weapons.IndexOf(myFactory.GetAbility(elementType)) == -1)
+                    playerWC.weapons.Add(myFactory.GetAbility(elementType));
                 playerWC.UpdateWeapons();
             }
             Destroy(powerUp);
+            duration = 0;
+            canPickup = false;
         }
-        Debug.Log(duration);
         if (duration != 0)
         {
             duration -= 1;
@@ -59,8 +62,9 @@ public class ElementPickup : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Powerup") //If colliding with any tag with Player
+        if (collision.gameObject.tag == "Powerup" && !canPickup) //If colliding with any tag with Player
         {
+            Vector3 currentPos = this.transform.position;
             elementType = collision.gameObject.GetComponent<ElementTyping>().elementType;
             Debug.Log(elementType);
             canPickup = true;
