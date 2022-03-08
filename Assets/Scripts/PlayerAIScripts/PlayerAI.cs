a1cdef9f77a2e258105fb8d1310c39f5ea2d3132
klluu <kevinluu228@gmail.com>
2022-03-07 22:44:06 -0800

    Update PlayerAI.cs

--- Assets/Scripts/PlayerAIScripts/PlayerAI.cs
+++ Assets/Scripts/PlayerAIScripts/PlayerAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
-
+using ElementFactory;
 
 // Kevin Luu
 public class PlayerAI : MonoBehaviour
@@ -11,17 +11,24 @@ public class PlayerAI : MonoBehaviour
     private NavMeshAgent _playerAI;
     private GameObject _player;
 
-    private int _health;
-    private Slider _healthSlider;
+    private int _health; // Not referenced yet
+    private Slider _healthSlider; // Not referenced yet
+
+    private PlayerWeaponController _playerWC;
 
     private GameObject[] _enemies;
+
+    private List<Ability> _AIWeapons = new List<Ability>();
+    private AbilityFactory myFactory = new AbilityFactory();
+
+    private bool _reloading = false;
 
     // Player States
     private enum PLAYER_STATE
     {
         Follow,
+        Look,
         Attack,
-        Evade,
     }
 
     private PLAYER_STATE _playerState;
@@ -32,6 +39,8 @@ public class PlayerAI : MonoBehaviour
         _player = GameManager.GManager.GetPlayers()[0];
         // Find enemies
         _enemies = GetEnemies();
+        // Adds weapons to list
+        _AIWeapons.Add(myFactory.GetAbility("aielement"));
     }
 
     
@@ -80,15 +89,50 @@ public class PlayerAI : MonoBehaviour
         if (Physics.Raycast(this.transform.position, rayToTarget, out raycastInfo))
         {
             //WRONG I NEED TO FIX TO FIND ALL ENEMIES
-            if (raycastInfo.transform.gameObject.tag == "Enemy" && Vector3.Distance(this.transform.position, _player.transform.position) < 10)
+            if (raycastInfo.transform.gameObject.tag == "Enemy" && Vector3.Distance(this.transform.position, ClosestEnemy().transform.position) < 10)
                 return true;
         }
         return false;
     }
 
+    // Loops to find the closest enemy to the playerAI
+    private GameObject ClosestEnemy()
+    {
+        GameObject closestEnemy = null;
+        float minEnemyDistance = 10000f;
+
+        for (int i = 0; i < _enemies.Length; i++)
+        {
+            float iterEnemyDistance = Vector3.Distance(this.transform.position, _enemies[i].transform.position);
+            
+            if (iterEnemyDistance < minEnemyDistance)
+            {
+                minEnemyDistance = iterEnemyDistance;
+                closestEnemy = _enemies[i];
+            }
+        }
+
+        return closestEnemy;
+    }
+
+    private void LookAtClosestEnemy()
+    {
+        _playerAI.transform.LookAt(ClosestEnemy().transform.position);
+    }
+
     // If enemy is in range, AI attacks using element
     void Attack()
     {
-        
+        if (!_reloading)
+        {
+            _AIWeapons[0].useAbility(this.transform.position,this.transform.forward);
+            _reloading = true;
+            StartCoroutine(Reload());
+        }
+    }
+
+    private IEnumerator Reload(){
+          yield return new WaitForSeconds(1);   //or however long you want the reload time to be
+          _reloading = false;
     }
 }
